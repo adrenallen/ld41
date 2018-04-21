972fa509a01242f05b46ac267927510605e9fafe
Garrett Allen <gigpop21@gmail.com>
2018-04-21 01:52:28 -0400

    color hints and level transitions. level code generic-ized (?) to be reusable

--- levels/BaseLevel.gd
+++ levels/BaseLevel.gd
@@ -1,15 +1,46 @@
-extends Node
+extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var victoryDoorOpen = false
+var nextLevel = "level1"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_process(true)
+	build_hint_tile_map()
+	
+#builds the hint tile map so playuer knows where to put colors
+func build_hint_tile_map():
+	for cell in $VictoryTileMap.get_used_cells():
+		#MY EYES, THEY BLEED
+		$HintTileMap.set_cell(cell.x, cell.y, $HintTileMap.tile_set.find_tile_by_name($HintTileMap.tile_set.tile_get_name($VictoryTileMap.get_cell(cell[0], cell[1])) + "hint"))
+	
+func next_level(binds):
+	global.goto_scene("res://levels/"+nextLevel+".tscn")
+	
+func _process(delta):
+	if(!victoryDoorOpen && check_for_win_condition()):
+		open_victory_door()
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+# gets the tile coords based on game coords
+func get_tile_coords_by_game_coords(x,y):
+	return Vector2(round(x/$ActiveTileMap.cell_size.x), round(y/$ActiveTileMap.cell_size.y))
+	
+#converts the tile at this position to the specified color
+func convert_tile_to_color(x,y,color):
+	var tile = get_tile_coords_by_game_coords(x,y)
+	$ActiveTileMap.set_cell(tile.x, tile.y, $ActiveTileMap.tile_set.find_tile_by_name(color+"tile"))
+
+#Check if our player's tile map matches victory conditions
+func check_for_win_condition():
+	for cell in $VictoryTileMap.get_used_cells():
+		if $ActiveTileMap.get_cell(cell[0], cell[1])  != $VictoryTileMap.get_cell(cell[0], cell[1]):
+			return false
+			
+	return true;
+
+func open_victory_door():
+	victoryDoorOpen = true
+	$ActiveTileMap.set_cell(7, 0, $ActiveTileMap.tile_set.find_tile_by_name("dooropen_left"))
+	$ActiveTileMap.set_cell(8, 0, $ActiveTileMap.tile_set.find_tile_by_name("dooropen_right"))
+	get_owner().get_node("Walls/DoorWall/CollisionShape2D").disabled = true
+	$Area2D.connect("body_entered", self, "next_level", [], CONNECT_ONESHOT)
+	
--- levels/level1.gd
+++ levels/level1.gd
@@ -1,43 +1,12 @@
-extends Node2D
+extends "BaseLevel.gd"
 
-var victoryDoorOpen = false
-var nextLevel = "level2"
 
 func _ready():
-	
-#	$TileMap.set_cell(0,0,$TileMap.tile_set.find_tile_by_name("redtile"))
+	nextLevel = "level2"
 	set_process(true)
 	
 	
-	
-func next_level(binds):
-	global.goto_scene("res://levels/"+nextLevel+".tscn")
-	
 func _process(delta):
-	if(!victoryDoorOpen && check_for_win_condition()):
-		open_victory_door()
-
-# gets the tile coords based on game coords
-func get_tile_coords_by_game_coords(x,y):
-	return Vector2(round(x/$ActiveTileMap.cell_size.x), round(y/$ActiveTileMap.cell_size.y))
+	pass	
 	
-#converts the tile at this position to the specified color
-func convert_tile_to_color(x,y,color):
-	var tile = get_tile_coords_by_game_coords(x,y)
-	$ActiveTileMap.set_cell(tile.x, tile.y, $ActiveTileMap.tile_set.find_tile_by_name(color+"tile"))
-
-#Check if our player's tile map matches victory conditions
-func check_for_win_condition():
-	for cell in $VictoryTileMap.get_used_cells():
-		if $ActiveTileMap.get_cell(cell[0], cell[1])  != $VictoryTileMap.get_cell(cell[0], cell[1]):
-			return false
-			
-	return true;
-
-func open_victory_door():
-	victoryDoorOpen = true
-	$ActiveTileMap.set_cell(7, 0, $ActiveTileMap.tile_set.find_tile_by_name("dooropen_left"))
-	$ActiveTileMap.set_cell(8, 0, $ActiveTileMap.tile_set.find_tile_by_name("dooropen_right"))
-	get_owner().get_node("Walls/DoorWall/CollisionShape2D").disabled = true
-	$Area2D.connect("body_entered", self, "next_level", [], CONNECT_ONESHOT)
 	
