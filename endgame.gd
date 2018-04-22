bf1baa2b299cd5096bb43e8f003d23f437126a6e
Garrett Allen <gigpop21@gmail.com>
2018-04-22 10:28:27 -0400

    intro screen and instructions

--- endgame.gd
+++ endgame.gd
@@ -1,18 +1,37 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var scorePointer = 0
+
 func _enter_tree():
 	global.clear_leftover_enemies()
 
 func _ready():
-	$Button.connect("button_down", self, "start_over", [], CONNECT_ONESHOT)
 	
+	#REMOVE THIS!
+	global.playerScore = 500
+	
+	$PlayAgainButton.connect("button_down", self, "start_over", [], CONNECT_ONESHOT)
+	$ScoreAddTimer.connect("timeout", self, "add_to_score_label")
 	
 func start_over():
 	global.playerHealth = global.maxPlayerHealth
 	global.goto_scene("res://levels/level1.tscn")
+
+func add_to_score_label():
+	
+	scorePointer += round(global.playerScore/20)
+	
+	if(scorePointer >= global.playerScore):
+		$FinalScoreStretch/FinalScoreValue.text = str(global.playerScore)
+		$ScoreAddTimer.disconnect("timeout", self, "add_to_score_label")
+		return
+		
+	$FinalScoreStretch/FinalScoreValue.text = str(scorePointer)
+	
+	
+	
+	
+	
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
---
+++ titlescreen.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	$Container/NewGameButton.connect("pressed", self, "start_new_game")
+	$Container/HowToPlayButton.connect("pressed", self, "toggle_how_to_play")
+	$HowToPlayPanel/HowToPlayCloseButton.connect("pressed", self, "toggle_how_to_play")
+
+func start_new_game():
+	global.goto_scene("res://levels/level1.tscn")
+	
+func toggle_how_to_play():
+	$HowToPlayPanel.visible = !$HowToPlayPanel.visible
