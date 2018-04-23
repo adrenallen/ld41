3915ae7bdd8777d381ac289cd17fd6e2fc311912
Garrett Allen <gigpop21@gmail.com>
2018-04-22 20:48:25 -0400

    lose game sound :D

--- ui.gd
+++ ui.gd
@@ -14,7 +14,7 @@ func lose_game():
 	var endSceneInstance = endScene.instance()
 	global.get_tile_map_container(self).get_owner().get_node("Music").stop()
 	$AnimationPlayer.stop()
-	$AnimationPlayer.queue("loseGame")
+	$LoseGameSound.play(0)
 	endSceneInstance.z_index = 4050
 	$ProgressBar.visible = false
 	$ScoreBox.visible = false
